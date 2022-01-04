f840d27f59963ec0f6a949b2beaeb6dfa5a62b7c
Marco Tulio <marcotulio.viana@yahoo.com.br>
2022-01-04 14:46:25 -0300

    Criando operação de instaciação

--- src/main/java/com/mark/cursomc/CursomcApplication.java
+++ src/main/java/com/mark/cursomc/CursomcApplication.java
@@ -1,19 +1,35 @@
 package com.mark.cursomc;
 
+import com.mark.domain.Categoria;
+import com.mark.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 @ComponentScan("com.mark.resource")
 @EntityScan("com.mark.domain")
 @EnableJpaRepositories("com.mark.repositories")
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,"Informatica");
+		Categoria cat2 = new Categoria(null,"Escritorio");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+	}
 }
