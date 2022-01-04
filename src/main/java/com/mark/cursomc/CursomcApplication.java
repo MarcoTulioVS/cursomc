15595b6da407691580d181e526b56efd13a42b4c
Marco Tulio <marcotulio.viana@yahoo.com.br>
2022-01-04 11:39:16 -0300

    testando o REST

--- src/main/java/com/mark/cursomc/CursomcApplication.java
+++ src/main/java/com/mark/cursomc/CursomcApplication.java
@@ -2,8 +2,10 @@ package com.mark.cursomc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.mark.resource")
 public class CursomcApplication {
 
 	public static void main(String[] args) {
