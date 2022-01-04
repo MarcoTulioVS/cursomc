59e6fb107827206554082d3efb634e05cc0c6999
Marco Tulio <marcotulio.viana@yahoo.com.br>
2022-01-04 14:37:16 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/mark/repositories/CategoriaRepository.java
@@ -0,0 +1,10 @@
+package com.mark.repositories;
+
+import com.mark.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria,Integer> {
+
+}
--- src/main/java/com/mark/resource/CategoriaResource.java
+++ src/main/java/com/mark/resource/CategoriaResource.java
@@ -1,7 +1,12 @@
 package com.mark.resource;
 
 import com.mark.domain.Categoria;
+import com.mark.services.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,16 +15,15 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value="/categorias")
+@ComponentScan("com.mark.services")
 public class CategoriaResource {
 
-    @GetMapping
-    public List<Categoria> listar(){
-        Categoria cat1 = new Categoria(1,"Informatica");
-        Categoria cat2 = new Categoria(2,"Escritorio");
+    @Autowired
+    private CategoriaService service;
 
-        List<Categoria> lista = new ArrayList<>();
-        lista.add(cat1);
-        lista.add(cat2);
-        return lista;
+    @GetMapping("/{id}")
+    public ResponseEntity<?> find(@PathVariable Integer id){
+        Categoria obj = service.buscar(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
